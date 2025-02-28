8d6d69ddef413e78719c2c7a1efb549b19ed7cf9
hanseongeon <gkstjddjs08@naver.com>
2025-03-01 01:24:52 +0900

    start

---
+++ backend/src/main/java/com/example/eonmusic/EonmusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.eonmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EonmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EonmusicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/eonmusic/EonmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eonmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EonmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
