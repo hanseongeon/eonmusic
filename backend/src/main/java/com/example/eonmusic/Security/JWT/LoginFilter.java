1de3cf18ef4cd2760dbdbbf6680bdfd97de51e50
hanseongeon <gkstjddjs08@naver.com>
2025-05-19 22:41:08 +0900

    JWT토큰 구현중

--- backend/src/main/java/com/example/eonmusic/Security/JWT/LoginFilter.java
+++ backend/src/main/java/com/example/eonmusic/Security/JWT/LoginFilter.java
@@ -1,18 +1,38 @@
 package com.example.eonmusic.Security.JWT;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
+
 @RequiredArgsConstructor
 public class LoginFilter  extends UsernamePasswordAuthenticationFilter {
-private final AuthenticationManager authenticationManager;
+private  AuthenticationManager authenticationManager;
+private  JWTUtil jwtUtil;
 
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+@Value("${spring.jwt.accessTokenExpirationTime}")
+private Long accessTime;
+
+@Value("${spring.jwt.refreshTokenExpirationTime}")
+private Long refreshTime;
 public Authentication attemptAuthentication (HttpServletRequest request, HttpServletResponse response){
     String userName = obtainUsername(request);
     String passWord = obtainPassword(request);
@@ -25,12 +45,37 @@ public Authentication attemptAuthentication (HttpServletRequest request, HttpSer
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+        String userName = authentication.getName();
 
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        //토큰 생성
+        String accessToken = jwtUtil.createJwt("access",userName,role,accessTime );
+        String refreshToken = jwtUtil.createJwt("refresh",userName,role,refreshTime);
+
+        //응답 설정
+        response.setHeader("access", accessToken);
+        response.addCookie(createCookie("refresh", refreshToken));
+        response.setStatus(HttpStatus.OK.value());
     }
 
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
 
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        //cookie.setSecure(true);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
